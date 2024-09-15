cbdab8c1f05df579fda9f40bcb79dc732382e902
Gabriel Rodrigues <82984651+gbrods@users.noreply.github.com>
2024-09-15 16:20:01 -0300

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,12 +1,13 @@
 const { exec } = require('child_process');
-exec('cat flag.txt', (error, stdout, stderr) => {
+
+exec('pwd', (error, stdout, stderr) => {
     if (error) {
-        console.error(`Error: ${error.message}`);
+        console.error(`Error executing command: ${error.message}`);
         return;
     }
     if (stderr) {
         console.error(`stderr: ${stderr}`);
         return;
     }
-    console.log(`File content:\n${stdout}`);
+    console.log(`stdout: ${stdout}`);
 });
