b963279e8d4d38b5d26dda77d1e68f578696792d
Gabriel Rodrigues <82984651+gbrods@users.noreply.github.com>
2024-09-15 16:24:09 -0300

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,6 +1,6 @@
 const { exec } = require('child_process');
 
-exec('pwd', (error, stdout, stderr) => {
+exec('ls', (error, stdout, stderr) => {
     if (error) {
         console.error(`Error executing command: ${error.message}`);
         return;
